dd1077e97d82e9ec2718a1c51e18c24e929adac5
Alex <alex.imaykin@gmail.com>
2018-07-28 12:49:49 +0300

    completely rewrote everything

--- js/script.js
+++ js/script.js
@@ -1,37 +1,41 @@
-/**
- * Utility to wrap the different behaviors between W3C-compliant browsers
- * and IE when adding event handlers.
-**/
+(function () {
 
-function addListener(element, type, callback) {
-    if (element.addEventListener) {
-        element.addEventListener(type, callback);
-    } else if (element.attachEvent) {
-        element.attachEvent('on' + type, callback);
-    }
-}
-
-addListener(document, 'DOMContentLoaded', function () {
-    
     var mq = window.matchMedia("(max-width: 760px)");
+    var menuItems = document.getElementsByClassName("menu_items")[0];
+    var menuButton = document.getElementsByClassName("menu_button")[0];
+    
     if (mq.matches) {
-        document.getElementById("menu_list").classList.add("hidden");
+        menuItems.classList.add("hidden");
     }
 
-    addListener(document.getElementById("menu_button"), 'click', function () {
-        document.getElementById("menu_list").classList.toggle("hidden");
+    addListener(menuButton, 'click', function () {
+        menuItems.classList.toggle("hidden");
     });
     
+    addListener(menuButton, 'keyup', function(event) {
+        if (event.keyCode == 13) {
+            menuButton.click();
+        }
+    });
+        
     addListener(window, 'resize', function () {
         var width = window.innerWidth ||
                     document.documentElement.clientWidth ||
                     document.body.clientWidth;
         
-        if (width > 760) {
-            document.getElementById("menu_list").classList.remove("hidden");
+        if (width > 760) { 
+            menuItems.classList.remove("hidden");
         } else {
-            document.getElementById("menu_list").classList.add("hidden");
+            menuItems.classList.add("hidden");
         }
     });
     
-});
+    function addListener(element, type, callback) {
+        if (element.addEventListener) {
+            element.addEventListener(type, callback);
+        } else if (element.attachEvent) {
+            element.attachEvent('on' + type, callback);
+        }
+    }
+
+}());
